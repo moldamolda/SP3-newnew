2f66e6c238be918e95105dd9c1689a71616b5e3e
Mads Benjamin Ribberholt <benjamin.ribberholt@gmail.com>
2023-11-22 10:58:32 +0100

    TextUI opdateret 2.0

--- TextUI.java
+++ TextUI.java
@@ -1,2 +1,72 @@
-public class TextUI {
+public class TextUI extends FileIO{
+
+    public void displayMessage(String msg){
+        System.out.println(msg);
+    }
+
+
+    public void displayMovies() {
+        for (String[] s : movieList) {
+            System.out.println(s);
+        }
+    }
+
+    public void displaySeries() {
+        for (String[] s : SeriesList) {
+            System.out.println(s);
+        }
+    }
+
+    public void displayWatchedMovie(User user){
+        User name = new User("","");
+
+        System.out.println(name.getUserName() + "," + " These are the movies that you have watched:");
+
+        for(Movie s : user.getWatchMovieLaterList()){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a movie from the list? \n ** Y/N **");
+
+    }
+
+    public void displaySavedMovie(User user){
+        User name = new User("","");
+
+        System.out.println(name.getUserName()+ "," + " These are the movies you have saved for later viewing:");
+
+        for(Movie s : user.getWatchedMovieList()){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a movie from the list? \n ** Y/N **");
+
+    }
+
+    public void displayWatchedSeries(User user){
+        User name = new User("","");
+
+        System.out.println(name.getUserName()+ "," + " This is a list of shows that you have watched:");
+
+        for(Serie s : user.getWatchSerieList()){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a show from the list? \n ** Y/N **");
+
+    }
+
+    public void displaySavedSeries(User user){
+        User name = new User("","");
+
+        System.out.println(name.getUserName()+ "," + " This is a list of shows you have saved for later viewing:");
+
+        for(Serie s : user.getWatchSerieLaterList()){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a show from the list? \n ** Y/N **");
+
+    }
+
 }
